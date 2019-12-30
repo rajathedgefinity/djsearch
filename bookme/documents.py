4f8ac3837a4a7355559106b1524f30d4f36a2c6d
Rajath Kumar K S <rajathkumar.ks@edgefinity.in>
2019-12-30 13:23:49 +0530

    Search Working Only Need to Create a Index for ELSearch Kibana

---
+++ bookme/documents.py
@@ -0,0 +1,27 @@
+from django_elasticsearch_dsl import Document
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import connections, Search
+from django_elasticsearch_dsl.registries import registry
+from .models import Book
+
+client = Elasticsearch()
+my_search = Search(using=client)
+connections.create_connection()
+
+@registry.register_document
+class BookDocument(Document):
+    class Index:
+        name = 'book'
+        settings = {
+            'number_of_shrads':1,
+            'number_of_replicas': 0
+        }
+
+    class Django:
+        model = Book
+        fields = ['title','isbn','category']
+
+def search(title):
+    query = my_search.query("match", title=title)
+    response = query.execute()
+    return response
--- bookme/models.py
+++ bookme/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return "Book Title: {}".format(self.title)
